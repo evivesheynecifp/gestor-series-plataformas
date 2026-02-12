3595ab8f4c05fdb47f67725d2b154e592556ce3f
evivesheynecifp <235907408+evivesheynecifp@users.noreply.github.com>
2026-02-12 19:52:05 +0100

    Update for tomcat functionality

--- gestor-series-plataformes/src/main/java/ifc33b/dwesc/gestor_series_plataformes/GestorSeriesPlataformesApplication.java
+++ gestor-series-plataformes/src/main/java/ifc33b/dwesc/gestor_series_plataformes/GestorSeriesPlataformesApplication.java
@@ -2,12 +2,19 @@ package ifc33b.dwesc.gestor_series_plataformes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GestorSeriesPlataformesApplication {
+public class GestorSeriesPlataformesApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestorSeriesPlataformesApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GestorSeriesPlataformesApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorSeriesPlataformesApplication.class, args);
+    }
 
 }
