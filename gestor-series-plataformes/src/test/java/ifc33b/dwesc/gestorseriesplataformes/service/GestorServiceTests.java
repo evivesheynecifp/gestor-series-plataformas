3a119903a0b9c6c1996034847e3900537df4f746
evivesheynecifp <235907408+evivesheynecifp@users.noreply.github.com>
2026-02-13 11:37:10 +0100

    Fix name

--- gestor-series-plataformes/src/test/java/ifc33b/dwesc/gestorseriesplataformes/service/GestorServiceTests.java
+++ gestor-series-plataformes/src/test/java/ifc33b/dwesc/gestorseriesplataformes/service/GestorServiceTests.java
@@ -22,7 +22,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class GestorServiceTest {
+class GestorServiceTests {
 
     @Mock
     private PlataformaRepository plataformaRepository;
