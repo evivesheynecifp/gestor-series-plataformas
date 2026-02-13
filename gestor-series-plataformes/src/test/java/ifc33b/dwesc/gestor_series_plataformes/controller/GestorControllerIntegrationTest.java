4f3e3f250e7425ef0fc9db2f837f7ff01967c55c
evivesheynecifp <235907408+evivesheynecifp@users.noreply.github.com>
2026-02-13 09:10:08 +0100

    Modify controller

--- gestor-series-plataformes/src/test/java/ifc33b/dwesc/gestor_series_plataformes/controller/GestorControllerIntegrationTest.java
+++ gestor-series-plataformes/src/test/java/ifc33b/dwesc/gestor_series_plataformes/controller/GestorControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
 class GestorControllerIntegrationTest {
 
     @Autowired
