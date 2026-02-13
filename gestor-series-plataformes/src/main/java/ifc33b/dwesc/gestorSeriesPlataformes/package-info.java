b9cb40046812d0f1c258c8fccc6d552fa986b9b3
evivesheynecifp <235907408+evivesheynecifp@users.noreply.github.com>
2026-02-13 11:10:38 +0100

    Add Javadoc package-info.java
    May be wrong, i dont foking know

---
+++ gestor-series-plataformes/src/main/java/ifc33b/dwesc/gestorSeriesPlataformes/package-info.java
@@ -0,0 +1,7 @@
+/** **
+ * Proyecto: Gestor de Series y Plataformas
+ * Descripción: Aplicación para gestionar series de televisión y las plataformas donde están disponibles.
+ * Autor: Erik Vives von Heyne
+ * Fecha: 13/02/2026
+ *** */
+package ifc33b.dwesc.gestorseriesplataformes;
